60cda19074c1e34c9ffaaaf6dde67eeafbb6c733
abdulghaffaransari <abdulghaffaransari9@gmail.com>
2024-12-20 21:48:19 +0100

    Retrieval.ipynb Added

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+if process_url_clicked:
+    pass
